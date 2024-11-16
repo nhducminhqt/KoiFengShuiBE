a347be0f2a4e69d3e76343b0b2c5bffff3c0c391
Nguyễn Hoàng Đức Minh <minhnhdse181549@fpt.edu.vn>
2024-11-16 14:20:02 +0700

    configuration

---
+++ KFS/src/main/java/org/example/kfs/configuaration/CustomJwtDecoder.java
@@ -0,0 +1,49 @@
+package org.example.kfs.configuaration;
+
+import com.google.api.client.util.Value;
+import com.nimbusds.jose.JOSEException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.text.ParseException;
+import java.util.Objects;
+
+public class CustomJwtDecoder implements JwtDecoder {
+    @Value("${jwt.signerKey}")
+    private String signerKey;
+
+    @Autowired
+    private AuthenticateService authenticationService;
+
+    private NimbusJwtDecoder nimbusJwtDecoder = null;
+
+    @Override
+    public Jwt decode(String token) throws JwtException {
+
+        try {
+            var response = authenticationService.introspect(
+                    IntrospectRequest.builder().token(token).build());
+
+            if (!response.isValid()) throw new JwtException("Token invalid");
+        } catch (JOSEException | ParseException e) {
+
+            throw new JwtException(e.getMessage());
+        }
+
+        if (Objects.isNull(nimbusJwtDecoder)) {
+            SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS512");
+            nimbusJwtDecoder = NimbusJwtDecoder.withSecretKey(secretKeySpec)
+                    .macAlgorithm(MacAlgorithm.HS512)
+                    .build();
+        }
+
+        return nimbusJwtDecoder.decode(token);
+    }
+
+
+}
---
+++ KFS/src/main/java/org/example/kfs/configuaration/EndPoint.java
@@ -0,0 +1,82 @@
+package org.example.kfs.configuaration;
+
+
+public class EndPoint {
+
+    public static final String[] ADMIN_GET_ENDPOINTS = {
+            "/animals", "/animals/animal-search", "/shelters", "/shelters/search-name", "/shelters/{search}",
+            "/colors", "shapes", "/animal-images", "/shelter-images", "colors/getAll-Colors",
+            "shapes/getAll-Shapes", "shapes/shape-search", "/users", "/users/search-name", "/roles",
+            "colors/search-colors", "api/bills"
+            ,"/users/new-users-today", "/users/new-users-this-week", "/users/new-users-this-month",
+            "/api/consultation-requests/completed-count",  "/posts/count", "/consultation-results/completed-count",
+            "/api/bills/total-income-this-month"
+    };
+
+    public static final String[] ADMIN_POST_ENDPOINTS = {
+            "/animals", "/shelters", "/colors", "/shapes", "/consulation-animal", "/consulation-shelter",
+            "/users{userId}/set-roles", "/animal-images/{id}", "/shelter-images/{id}", "/packages",
+            "/api/consultation-results/requestId/{requestId}",
+            "/api/consultation-animals/",
+            "/api/consultation-shelters/resultId/{resultId}/shelter-category-id/{shelterCategoryId}",
+    };
+
+    public static final String[] ADMIN_PUT_ENDPOINTS = {
+            "/animals/{id}", "/shelters/{id}", "/colors/{id}", "/shapes/{id}",
+            "/packages/{id}", "/api/consultation-results/send-email/{resultId}",
+            "/api/consultation-results/{id}",
+            "/api/consultation-request-details/{id}",
+            "/api/consultation-requests"
+    };
+
+    public static final String[] ADMIN_DELETE_ENDPOINTS = {
+            "/animals/{id}", "/shelters/{id}", "/colors/{id}", "/shapes/{id}", "/packages/{id}"
+    };
+
+    public static final String[] PUBLIC_ENDPOINTS = {
+            "/users", "/users/admin", "/auth/token", "/auth/introspect", "/auth/logout", "/auth/refresh",
+            "/auth/outbound/authentication", "/roles", "auth/token-email",
+            "/destinys/compatibility/{yearOfBirth}"
+    };
+
+    public static final String[] PUBLIC_GET_ENDPOINTS = {
+            "users/existByUserName", "users/existByEmail", "users/activate",
+            "/destinys/destiny/{yearOfBirth}", "/destinys/autoConsultation/{yearOfBirth}",
+            "api/vn_pay/create_vn_pay", "shapes/getAll-Shapes", "/api/number",
+            "animals/animalCategory", "colors/getAll-Colors", "/shelters/shelterCategory","api/direction","/api/bills/search"
+    };
+
+    public static final String[] USER_GET_ENDPOINTS = {
+            "/posts", "/posts/search-posts", "/post/comments", "/post/images",
+            "api/bills/{billId}", "/vn_pay/create_vn_pay","/search-posts/title","/search-posts/email",
+            "/animals/animal-destiny" , "/posts/search-posts/year", "/shelters/shelter-search", "/animals/animal-destiny-name", "/shelters/shelter-search-name"
+    };
+
+    public static final String[] USER_POST_ENDPOINTS = {
+            "users/reset-password", "users/forgot-password", "/posts", "/post/comments", "/post/images",
+            "api/bills/request/{requestId}/payments/{paymentId}",
+            "/api/consultation-requests",
+            "/api/consultation-request-details/request-id/{requestId}",
+            "users/update-password"
+    };
+
+    public static final String[] USER_PUT_ENDPOINTS = {
+            "/posts/{id}", "/post/comments/{id}", "/post/images/{id}", "api/bills/{id}", "/api/consultation-requests"
+    };
+
+    public static final String[] USER_DELETE_ENDPOINTS = {
+            "/posts/{id}", "/post/comments/{id}", "/post/images/{id}"
+    };
+
+    public static final String[] BOTH_GET_ENDPOINTS = {
+            "api/bills/{id}/status",
+            "api/bills/{billId}",
+            "/destinys"
+
+    };
+
+    public static final String[] BOTH_DELETE_ENDPOINTS = {
+            "/posts/{id}",
+    };
+}
+
