2fe531b78cdddfbfa510c677837b70110aeba554
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2024-11-30 19:46:45 +0700

    update

--- KFS/src/main/java/org/example/kfs/entity/ConsultationCategory.java
+++ KFS/src/main/java/org/example/kfs/entity/ConsultationCategory.java
@@ -19,16 +19,17 @@ public class ConsultationCategory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "consultation_category_id", nullable = false)
     private Integer id;
+    @NotNull
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.INACTIVE;
 
     @Size(max = 100)
     @NotNull
     @Column(name = "name", nullable = false, length = 100)
     private String name;
 
-    @NotNull
-    @Column(name = "status")
-    @Enumerated(EnumType.STRING)
-    private Status status = Status.INACTIVE;
+
 
     @OneToMany(mappedBy = "consultationCategory", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
     private Set<ConsultationResult> consultationResults = new LinkedHashSet<>();
