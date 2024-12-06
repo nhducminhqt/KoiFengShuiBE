c7b29412dd3521eaf5e643120710955e69713737
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2024-12-06 11:31:37 +0700

    done entity

---
+++ KFS/src/main/java/org/example/kfs/entity/ShelterImage.java
@@ -0,0 +1,31 @@
+package org.example.kfs.entity;
+
+import com.fengshuisystem.demo.entity.enums.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Nationalized;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+public class ShelterImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "shelter_image_id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, optional = false)
+    @JoinColumn(name = "shelter_category_id", nullable = false)
+    private ShelterCategory shelterCategory;
+
+    @Lob
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+}
+
