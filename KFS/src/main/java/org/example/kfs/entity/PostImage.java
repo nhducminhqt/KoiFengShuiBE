77daa214fc7a8eaa7e39fc078994183513cb52ff
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2024-12-06 11:24:39 +0700

    add Post Cate and Image

---
+++ KFS/src/main/java/org/example/kfs/entity/PostImage.java
@@ -0,0 +1,35 @@
+package org.example.kfs.entity;
+
+
+import com.fengshuisystem.demo.entity.enums.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Nationalized;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+public class PostImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_image_id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, optional = false)
+    @JoinColumn(name = "post_post_id")
+    private Post post;
+
+    @Lob
+    @Size(max = 255)
+    @NotNull
+    @Nationalized
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+}
