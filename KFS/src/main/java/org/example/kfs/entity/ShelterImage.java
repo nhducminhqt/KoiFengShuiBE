b3b44a8b7fc6513b3c33949ee4c8e382c294a46e
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2024-12-07 14:48:17 +0700

    add

--- KFS/src/main/java/org/example/kfs/entity/ShelterImage.java
+++ KFS/src/main/java/org/example/kfs/entity/ShelterImage.java
@@ -14,15 +14,16 @@ import java.time.Instant;
 @Setter
 @Entity
 public class ShelterImage {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "shelter_image_id", nullable = false)
-    private Integer id;
+
 
     @NotNull
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, optional = false)
     @JoinColumn(name = "shelter_category_id", nullable = false)
     private ShelterCategory shelterCategory;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "shelter_image_id", nullable = false)
+    private Integer id;
 
     @Lob
     @Column(name = "image_url", nullable = false)
