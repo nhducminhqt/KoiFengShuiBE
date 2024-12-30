9ea669632740289b36fc182a39b8bcf4b1cc0d5d
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2024-12-30 13:52:15 +0700

    add controller

---
+++ KFS/src/main/java/org/example/kfs/controller/PostImageController.java
@@ -0,0 +1,20 @@
+package org.example.kfs.controller;
+
+import com.fengshuisystem.demo.dto.*;
+import com.fengshuisystem.demo.service.PostImageService;
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/post/images")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class PostImageController {
+    PostImageService postImageService;
+
+}
