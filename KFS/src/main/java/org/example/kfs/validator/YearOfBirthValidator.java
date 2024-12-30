9e44e6d07dac1b0bf0c3bdc697551ab2d2546c8a
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2024-12-30 16:31:50 +0700

    add validator

---
+++ KFS/src/main/java/org/example/kfs/validator/YearOfBirthValidator.java
@@ -0,0 +1,17 @@
+package org.example.kfs.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.time.Year;
+
+public class YearOfBirthValidator implements ConstraintValidator<ValidYearOfBirth, Integer> {
+
+    @Override
+    public boolean isValid(Integer yearOfBirth, ConstraintValidatorContext context) {
+        if (yearOfBirth == null) {
+            return false; // or true, depending on whether you want null to be valid
+        }
+        int currentYear = Year.now().getValue();
+        return yearOfBirth >= 1900 && yearOfBirth <= currentYear;
+    }
+}
